eb35b0d05c207e216ddeb4695e602c6293fabc73
DyonathanSantos <dyonathanssantos@gmail.com>
2025-10-19 14:23:07 -0300

    Bug in git and I lost all commit

---
+++ app/create_db.py
@@ -0,0 +1,58 @@
+import sqlite3
+
+def connect():
+    return sqlite3.connect('base.db')
+
+con = connect() # open connection
+cur = con.cursor()
+
+
+cur.execute('''CREATE TABLE IF NOT EXISTS stock( 
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            product TEXT NOT NULL UNIQUE, category TEXT, quantity INTEGER DEFAULT 0, 
+            buy REAL DEFAULT 0.0,
+            sell REAL DEFAULT 0.0
+             )''') # Stock
+
+cur.execute('CREATE INDEX IF NOT EXISTS idx_stock_product ON stock(product)') # index of stock for fast find
+
+cur.execute('''CREATE TABLE IF NOT EXISTS comanda (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            customer TEXT,
+            status TEXT DEFAULT 'open',
+            data TEXT DEFAULT (datetime('now','localtime')))''') # Comanda for customer register    
+
+cur.execute('CREATE INDEX IF NOT EXISTS idx_comanda_id ON comanda(id)') # index of comanda
+
+cur.execute('''CREATE TABLE IF NOT EXISTS item_comanda (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            id_comanda INTEGER,
+            id_product INTEGER,
+            product TEXT,
+            quantity INTEGER,
+            price REAL,
+            FOREIGN KEY (id_comanda) REFERENCES comandas(id),
+            FOREIGN KEY (id_product) REFERENCES product(id)
+            )''') # save the product in comanda for late list
+
+cur.execute('''CREATE TABLE IF NOT EXISTS users(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE,
+            password_hash TEXT,
+            role TEXT -- admin or staff
+            )''') # Table of employee
+
+cur.execute('''CREATE TABLE IF NOT EXISTS sells (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            data TEXT DEFAULT (datetime('now','localtime')),
+            product TEXT,
+            quantity INTEGER,
+            price REAL,
+            total REAL
+            )''') # for sell in day ou mouth 
+
+cur.execute('CREATE TABLE IF NOT EXISTS logs(user TEXT, action TEXT)') # Table for register logs
+
+cur.execute('CREATE INDEX IF NOT EXISTS idx_username_users ON users(username)') # index of USERS
+
+con.close() #close connection
---
+++ app/teste.py
@@ -0,0 +1,7 @@
+#add - stock check  // add comanda - check // all see -- check // update stock -- check // 
+
+from crud import *
+import pandas as pd
+
+
+close_comanda(1,'ADM')
